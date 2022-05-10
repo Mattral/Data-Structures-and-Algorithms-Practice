b166f1336cebd5baa34446f2cfdd64da2447db9c
Mattral <88831350+Mattral@users.noreply.github.com>
2022-05-10 20:55:28 +0630

    Add files via upload

---
+++ Finding+all+Subarrays.cpp
@@ -0,0 +1,82 @@
+// Question: listing out all subarrays
+
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+
+#define pb push_back
+#define ll long long int
+
+using namespace std;
+
+/*-------------------------------------------
+--------------Rohith's Solution--------------
+---------------------------------------------*/
+
+#define maxX(a,b) (a>b?a:b)
+#define minX(a,b) (a<b?a:b)
+#define isEven(a) ((a%2==0)?1:0)
+#define isOdd(a) ((a%2==0)?0:1)
+#define isNeg(a) ((a<0)?1:0)
+#define loop(n) for(int i = 0; i < n; i++)
+#define loop2(n) for(int j = 0; j < n; j++)
+#define abs(a) ((a<0)?-a:a)
+
+
+void listoutSubarrays(int a[], int n){
+
+    // first loop
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = i; j < n; j++){
+
+            // in the second loop
+            // every i,j pair is a configuration -> subarray 
+
+            // printing subarray
+            int sum = 0;
+            for(int k = i; k <= j; k++){
+                sum += a[k]; 
+                cout << a[k] << " ";
+            }
+
+            cout << "     Sum: " << sum << endl;
+        }
+    }
+}
+
+void Sol(){
+    int n;
+    cin >> n;
+    int a[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    listoutSubarrays(a, n);
+}
+
+void RepSolution(){
+    int t;
+    cin >> t;
+    while(t!=0){
+        Sol();
+        t--;
+    }
+}
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //RepSolution();
+    Sol();
+    return 0;
+}
